0c6e5321c8ab8fd80a86e30a994af332678a008d
Jennifer Oh <ohjennifer.90@gmail.com>
2019-05-21 10:22:32 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :sufficiently_clickbaity
+  
+  def sufficiently_clickbaity
+    if self.title
+      if !self.title.include?("Won't Believe" || "Secret" || "Guess" || "Top [number]")
+      errors.add(:title, "Not clickbait-y enough")
+      end
+    end  
+  end
+
 end
